de7e9adb0a8abc48547c9cca55177ff786c798ea
Luigi <l.avila.uvg@gmail.com>
2023-10-30 17:49:44 -0400

    feat: Update dependencies for android 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.luigidev.linkthread"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.luigidev.linkthread"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
